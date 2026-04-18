32ba89461300824525ea090dc3f26b998d723ee9
ThiagoS5 <thimarqueti@gmail.com>
2026-04-18 18:26:47 -0300

    feat: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("O elemento #root não foi encontrado no arquivo HTML");
+}
+
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
